3a8106fb9a3f749ae3d85851ad86c834eb0a324e
Thomas Peace <thomasjpeace29@gmail.com>
2024-06-05 22:15:16 +0100

    create saveBooking method, the last MVP route

--- src/main/java/com/example/airline_api/services/BookingService.java
+++ src/main/java/com/example/airline_api/services/BookingService.java
@@ -1,6 +1,11 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Booking;
+import com.example.airline_api.models.BookingDTO;
+import com.example.airline_api.models.Flight;
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.BookingRepository;
+import org.hibernate.property.access.internal.PropertyAccessStrategyResolverStandardImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +15,18 @@ public class BookingService {
     @Autowired
     BookingRepository bookingRepository;
 
-    public void saveBooking()
+    @Autowired
+    FlightService flightService;
+
+    @Autowired
+    PassengerService passengerService;
+
+    public Booking saveBooking(BookingDTO bookingDTO){
+        Flight flight = flightService.findSingleFlight(bookingDTO.getFlightId()).get();
+        Passenger passenger = passengerService.getSinglePassenger(bookingDTO.getPassengerId()).get();
+
+        Booking createdBooking = new Booking(flight, passenger, bookingDTO.getSeatNumber());
+
+        return bookingRepository.save(createdBooking);
+    }
 }
