7e0ae1040046e7d64f25c395c80796481d3f2e43
Thomas Peace <thomasjpeace29@gmail.com>
2024-06-05 21:12:58 +0100

    add MVP routes 4-6

--- src/main/java/com/example/airline_api/services/PassengerService.java
+++ src/main/java/com/example/airline_api/services/PassengerService.java
@@ -1,4 +1,33 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Passenger;
+import com.example.airline_api.repositories.BookingRepository;
+import com.example.airline_api.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PassengerService {
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    public void savePassenger(Passenger passenger) {
+        passengerRepository.save(passenger);
+    }
+
+    public List<Passenger> getAllPassengers(){
+        return passengerRepository.findAll();
+    }
+
+    public Optional<Passenger> getSinglePassenger(long id){
+        return passengerRepository.findById(id);
+    }
+
 }
